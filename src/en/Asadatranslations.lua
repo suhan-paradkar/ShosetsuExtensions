2b4d8f3394547de54efaff421d0dc857a00ca94c
wisest_wizard <12suhangp34@gmail.com>
2022-12-02 15:06:03 +0530

    Add Asada Translations

---
+++ src/en/Asadatranslations.lua
@@ -0,0 +1,36 @@
+return Require("Madara")("https://asadatranslations.com",{
+	id = 9091,
+	name = "Asada Translations",
+	imageURL = "https://asadatranslations.com/wp-content/uploads/2020/05/asada-1.png",
+
+	novelListingURLPath = "novel",
+	shrinkURLNovel = "novel",
+	searchHasOper = true,
+
+    genres = {
+		"Action",
+        "Adventure",
+        "Comedy",
+        "Drama",
+        "Ecchi",
+        "Fantasy",
+        "Harem",
+        "Historical",
+        "Horror",
+        "Josei",
+        "Martial Arts",
+        "Mature",
+        "Mystery",
+        "Psychological",
+        "Romance",
+        "School Life",
+        "Sci-fi",
+        "Seinen",
+        "Shounen",
+        "Shounen Ai",
+        "Slice of life",
+        "Supernatural",
+        "Tragedy",
+        "Yaoi"
+	}
+})
