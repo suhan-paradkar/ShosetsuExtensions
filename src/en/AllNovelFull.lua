ee12ffa223f0b0846e9129ba869d835746438034
wisest_wizard <12suhangp34@gmail.com>
2022-12-03 08:24:19 +0530

    Add AllNovelFull

---
+++ src/en/AllNovelFull.lua
@@ -0,0 +1,12 @@
+return Require("NovelFull")("https://allnovelfull.com",{
+	id = 9006,
+	name = "AllNovelFull",
+	imageURL = "https://allnovelfull.com/web/logo.png",
+
+        meta_offset = 0,
+        ajax_hot = "/ajax-search?type=hot",
+	ajax_latest = "/ajax-search?type=latest",
+        ajax_chapters = "/ajax-chapter-option",
+        searchListSel = "list.list-truyen.col-xs-12",
+        searchTitleSel = ".truyen-title"
+})
