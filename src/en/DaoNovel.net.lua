10a87b6a224139fb5510a4bd6ab5ecce964e9337
Wisest_wizard <12suhangp34@gmail.com>
2023-11-22 11:16:14 +0530

    Add DaoNovel.net

---
+++ src/en/DaoNovel.net.lua
@@ -0,0 +1,10 @@
+-- {"id":900655555, "ver":"1.0.0","libVer":"1.0.0","author":"suhan-paradkar","dep":["NovelFull>=2.0.2"]}
+
+return Require("NovelFull")("https://daonovel.net/",{
+	id = 900655555,
+	name = "DaoNovel.net",
+	imageURL = "https://daonovel.net/img/logo.png",
+
+        searchListSel = "list.list-novel.col-xs-12",
+        searchTitleSel = ".novel-title"
+})
