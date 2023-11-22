12a6fde3f68182b568a963472b5486ba674228db
Wisest_wizard <12suhangp34@gmail.com>
2023-11-22 11:18:54 +0530

    A fix for DaoNovel.net (probably)

--- src/en/DaoNovel.net.lua
+++ src/en/DaoNovel.net.lua
@@ -1,6 +1,6 @@
--- {"id":900655555, "ver":"1.0.0","libVer":"1.0.0","author":"suhan-paradkar","dep":["NovelFull>=2.0.2"]}
+-- {"id":900655555, "ver":"1.0.1","libVer":"1.0.0","author":"suhan-paradkar","dep":["NovelFull>=2.0.2"]}
 
-return Require("NovelFull")("https://daonovel.net/",{
+return Require("NovelFull")("https://daonovel.net",{
 	id = 900655555,
 	name = "DaoNovel.net",
 	imageURL = "https://daonovel.net/img/logo.png",
